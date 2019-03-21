710fc3f96685995b70ab280bf641f393568bea85
DamienRiviere <damien@d-riviere.fr>
2019-03-21 14:43:32 +0100

    Réinstallation de Babel

--- webpack.config.js
+++ webpack.config.js
@@ -11,5 +11,19 @@ module.exports = {
     devServer: {
         contentBase: path.resolve(__dirname, "dist"),
         open: true
-    }
+    },
+    module: {
+        rules: [
+          {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+          }
+        ]
+      }
 }
