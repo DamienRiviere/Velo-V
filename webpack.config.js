713d3a760704061e744f756c4ecf61e206b0d2e2
DamienRiviere <damien@d-riviere.fr>
2019-04-08 11:34:51 +0200

    Modification config webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,25 @@
 const path = require("path");
 
 module.exports = {
-    mode: "production",
-    watch: true,
-    entry: "./src/index.js",
+    entry: {
+      app: './src/index.js'
+    },
     output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js"
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
     },
     devServer: {
-        contentBase: path.resolve(__dirname, "dist"),
+        contentBase: path.resolve(__dirname, 'dist'),
         open: true
     },
     module: {
-        rules: [
-          {
-            test: /\.m?js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                presets: ['@babel/preset-env']
-              }
-            }
+        rules: [{
+          test: /\.js?$/,
+          exclude: /node_modules/,
+          loaders: 'babel-loader',
+          query: {
+              presets: ['@babel/env']
           }
-        ]
-      }
+      }]
+    }
 }
